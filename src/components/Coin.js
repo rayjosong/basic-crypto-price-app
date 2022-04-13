9a3affe835cfcd3aa2b000cce25a9a6c167f02c9
rayjosong <rayjosong@gmail.com>
2022-04-13 21:27:20 +0800

    toFixed(2) for prices

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -5,7 +5,7 @@ function Coin({ name, icon, price, symbol }) {
     <div className="coin">
       <h1>Name: {name}</h1>
       <img src={icon} />
-      <h3>Price: {price}</h3>
+      <h3>Price: {price.toFixed(2)}</h3>
       <h3>Symbol: {symbol}</h3>
     </div>
   );
